4d628764fa72af42c05d630f8452f9ebd7ad1a93
Yauhescha <evgescha.maslov007@yandex.ru>
2023-03-24 20:29:39 +0300

    add login and  registration pages

---
+++ src/main/java/com/hescha/minijira/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.hescha.minijira.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String indexPage() {
+        return "redirect:/project";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
